ab71066a54858dacfae444dfd8dbacd3b011cb77
egorsmth <mattspring0@gmail.com>
2024-08-31 21:33:37 +0300

    now really assignment 1

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -66,7 +66,10 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            sidx = np.random.choice(np.arange(num_train), batch_size)
+            X_batch = X[sidx]
+            y_batch = y[sidx]
+            
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -81,7 +84,7 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            self.W -= grad * learning_rate
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -111,7 +114,7 @@ class LinearClassifier(object):
         ###########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        y_pred = np.argmax(X.dot(self.W), axis=1)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return y_pred
--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -23,10 +23,10 @@ def svm_loss_naive(W, X, y, reg):
     - gradient with respect to weights W; an array of same shape as W
     """
     dW = np.zeros(W.shape)  # initialize the gradient as zero
-
-    # compute the loss and the gradient
     num_classes = W.shape[1]
     num_train = X.shape[0]
+    # compute the loss and the gradient
+  
     loss = 0.0
     for i in range(num_train):
         scores = X[i].dot(W)
@@ -37,13 +37,16 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1  # note delta = 1
             if margin > 0:
                 loss += margin
-
+                dW[:, y[i]] -= X[i]
+                dW[:, j] += X[i]
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
     loss /= num_train
-
+    dW /= num_train
+    
     # Add regularization to the loss.
     loss += reg * np.sum(W * W)
+    dW += reg * (2.0 * W)
 
     #############################################################################
     # TODO:                                                                     #
@@ -55,8 +58,7 @@ def svm_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
-
+    
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
@@ -78,8 +80,17 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    scores = X.dot(W)
 
+    b = y.reshape(y.shape[0], 1)
+    select = np.arange(scores.shape[0])[:, None]
+    scores = scores - scores[select, b] + 1
+    scores[select, b] = 0
+    margins = np.maximum(0, scores)
+    loss = np.sum(margins)
+
+    loss /= X.shape[0]
+    loss += reg * np.sum(W * W)
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     #############################################################################
@@ -93,7 +104,13 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    margins[margins > 0] = 1
+    valid_count = np.sum(margins, axis=1)
+    margins[np.arange(margins.shape[0]),y ] -= valid_count
+    
+    dW = X.T.dot(margins)
+    dW /= X.shape[0]
+    dW += reg * (2.0 * W)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
